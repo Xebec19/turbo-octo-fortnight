cccce3fb3ce282cdbc495955dae0efbe4aee73d9
xebec19 <rohandeveloper106@gmail.com>
2024-05-23 00:32:38 +0530

    wip player

--- ball-physics/script.js
+++ ball-physics/script.js
@@ -1,1 +1,9 @@
-kaboom();
+kaboom({
+  global: true,
+  fullscreen: true,
+  clearColor: [0, 0, 0, 1],
+});
+
+loadSprite("tree1", "sprites/foliagePack_010.png");
+
+add([sprite("tree1"), pos(900, 40)]);
